e26a92c1d83461554a380c12459d3a51c5d34628
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-01 19:13:22 -0300

    chore: initializing project

---
+++ src/main/java/com/julio/billing_microservice/BillingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.julio.billing_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julio/billing_microservice/BillingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.billing_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
